7929d0afa541a65a3b8772ef6a851cb6beaae9c3
黄良运 <1136513099@qq.com>
2020-03-18 23:56:17 +0800

    318

---
+++ java/jsjxyxt-micro-service/training-service/src/test/java/com/javahly/trainingservice/TrainingServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.javahly.trainingservice;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
